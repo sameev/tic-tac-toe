fbb7b21bd4f6fa48d9c1a1993ea1114c8d7dc50b
Samee Vohra <samee.vohra@gmail.com>
2022-11-23 16:18:49 -0800

    board setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["Lobster", ...defaultTheme.fontFamily.sans]
+      }
+    },
+  },
+  plugins: [],
+}
